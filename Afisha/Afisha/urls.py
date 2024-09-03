e73e8d7a12525272bcb17b0936ceee638ecbbe9c
SultanovER <sae160305@gmail.com>
2024-09-03 18:21:27 +0600

    hw3

--- Afisha/Afisha/urls.py
+++ Afisha/Afisha/urls.py
@@ -5,13 +5,11 @@ from movie_app.views import MovieListView, DirectorListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/movie_app/movies/', views.movie_list_api_view),
-
+    path('api/v1/movie_app/movies/', views.movie_list_create_api_view),
     path('api/v1/movie_app/reviews/', views.review_list_api_view),
     path('api/v1/movie_app/directors/<int:id>/', views.director_detail_api_view),
     path('api/v1/movie_app/movies/<int:id>/', views.movie_detail_api_view),
     path('api/v1/movie_app/reviews/<int:id>/', views.review_detail_api_view),
     path('api/v1/movie_app/movies/reviews/', MovieListView.as_view(), name='movie-list-with-reviews'),
     path('api/v1/movie_app/directors/', DirectorListView.as_view(), name='director-list'),
-
 ]
--- Afisha/movie_app/views.py
+++ Afisha/movie_app/views.py
@@ -8,9 +8,11 @@ class DirectorListView(generics.ListAPIView):
     queryset = Director.objects.all()
     serializer_class = DirectorSerializer
 
+
 class MovieListView(generics.ListAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
 
 
 @api_view(['GET'])
@@ -19,11 +21,27 @@ def director_detail_api_view(request, id):
     data = DirectorSerializer(director).data
     return Response(data=data)
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def movie_detail_api_view(request, id):
-    movie = Movie.objects.get(id=id)
-    data = MovieSerializer(movie).data
-    return Response(data=data)
+    try:
+        movie = Movie.objects.get(id=id)
+    except Movie.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND, data={'error': 'Movie not found!'})
+    
+    if request.method == 'GET':
+        data = MovieSerializer(movie).data
+        return Response(data=data)
+    elif request.method == 'PUT':
+        movie.title = request.data.get("title")
+        movie.duration = request.data.get("duration")
+        movie.description = request.data.get("description")
+        movie.director = request.data.get("director")
+        movie.tags = request.data.get("tags")
+        movie.save()
+        return Response(data=MovieSerializer(movie).data, status=status.HTTP_201_CREATED)
+    elif request.method == 'DELETE':
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['GET'])
 def review_detail_api_view(request, id):
@@ -37,11 +55,30 @@ def director_list_api_view(request):
     list_ = DirectorSerializer(instance=directors, many=True).data
     return Response(data=list_, status=status.HTTP_200_OK)
 
-@api_view(http_method_names=['GET'])
-def movie_list_api_view(request):
-    movies = Movie.objects.all()
-    list_ = MovieSerializer(instance=movies, many=True).data
-    return Response(data=list_, status=status.HTTP_200_OK)
+@api_view(http_method_names=['GET', 'POST'])
+def movie_list_create_api_view(request):
+    if request.method == 'GET':
+        movies = Movie.objects.select_related('category').prefetch_related('tags', 'all_reviews').all()
+        list_ = MovieSerializer(instance=movies, many=True).data
+        return Response(data=list_, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        title = request.data.get("title")
+        duration = request.data.get("duration")
+        description = request.data.get("description")
+        director = request.data.get("director")
+        tags = request.data.get("tags")
+        
+        movie = Movie.objects.create(
+            title=title,
+            duration=duration,
+            description=description,
+            director=director.id
+        )
+        movie.tags.set(tags)
+        movie.save()
+        return Response(data=MovieSerializer(movie).data, status=status.HTTP_201_CREATED)
+    
+    
 
 @api_view(http_method_names=['GET'])
 def review_list_api_view(request):
